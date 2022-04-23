908da3e5081782380da1c3b1acbd491bb558d512
Nick Lally <nicklally@N040567-npla224.local>
2022-04-23 17:57:57 -0400

    addedFeatures2

--- cites.js
+++ cites.js
@@ -1,16 +1,53 @@
-var w = 960,
-    h = 500,
+var graph;
+var vis;
+
+function updateData() {
+	if (first==0) {
+		first=1;
+	}
+	else {
+    	d3.select("#old").remove();
+
+	}
+
+
+
+var w = 2000,
+    h = 1600,
     fill = d3.scale.category20();
 
-var vis = d3.select("#chart")
+vis = d3.select("#chart")
   .append("svg:svg")
     .attr("width", w)
-    .attr("height", h);
+    .attr("id","old")
+    .attr("height", h)
+      .attr("pointer-events", "all")
+  .append('svg:g')
+    .call(d3.behavior.zoom().on("zoom", redraw))
+  .append('svg:g');
 
-d3.json("cites.json", function(json) {
+  vis.append('svg:rect')
+    .attr('width', w)
+    .attr('height', h)
+    .attr('fill', 'white');
+
+function redraw() {
+  //console.log("here", d3.event.translate, d3.event.scale);
+  vis.attr("transform",
+      "translate(" + d3.event.translate + ")"
+      + " scale(" + d3.event.scale + ")");
+}
+
+
+var file="cites"+document.getElementById('id1').value+".json";
+console.log(file);
+
+
+d3.json(file, function(json) {
+
   var force = d3.layout.force()
-      .charge(-125)
-      .linkDistance(50)
+      .charge(-525)
+      .linkDistance(350)
       .nodes(json.nodes)
       .links(json.links)
       .size([w, h])
@@ -34,7 +71,7 @@ d3.json("cites.json", function(json) {
   	node.append("svg:circle")
       .attr("r", 5)
       .style("fill", function(d) { return fill(d.group); })
-      .call(force.drag);
+      .call(force.drag)
 
   node.append("svg:text")
     .attr("class", "nodetext")
@@ -55,8 +92,63 @@ d3.json("cites.json", function(json) {
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
-    
+
     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  graph = json;
   });
+
 });
 
+}
+
+var first=0;
+updateData();
+d3.selectAll("input").on("change", change);
+function change() {
+console.log("Change");
+console.log(this.value);
+
+updateData();
+}
+
+
+var optArray = [];
+for (var i = 0; i < graph.nodes.length - 1; i++) {
+    optArray.push(graph.nodes[i].name);
+}
+
+optArray = optArray.sort();
+
+$(function () {
+    $("#search").autocomplete({
+        source: optArray
+    });
+});
+
+
+function searchNode() {
+
+    //find the node
+
+    var selectedVal = document.getElementById('search').value;
+    var node = vis.selectAll(".node");
+	console.log(selectedVal);
+    if (selectedVal == "none") {
+        node.style("stroke", "white").style("stroke-width", "1");
+    } else {
+        var selected = node.filter(function (d, i) {
+            return d.name != selectedVal;
+        });
+        selected.style("opacity", "0");
+        var link = vis.selectAll(".link")
+        link.style("opacity", "0");
+        d3.selectAll(".node, .link").transition()
+            .duration(5000)
+            .style("opacity", 1);
+
+
+    }
+
+
+}
